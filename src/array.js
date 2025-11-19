3cf14b56aaa0c00d9b7925fc266d6b17d8f72c73
shrouk-mokhles <shroukm146@gmail.com>
2025-11-19 10:58:16 +0200

    loop array

--- src/array.js
+++ src/array.js
@@ -1,3 +1,8 @@
-const creiden = ["shrouq", "amir", "rana", "ahmed", "aya"];
-creiden[1] = "mohamed";
-console.log(creiden)
+// const creiden = ["shrouq", "amir", "rana", "ahmed", "aya"];
+// creiden[1] = "mohamed";
+// console.log(creiden)
+
+const users = [{ name: "shrouq", age: 28 }, { name: "amir", age: 25 }];
+for (let user of users) {
+    console.log(user.name,":" ,user.age)
+}
